5d6d86e867426e371dcf6cdbb15eeb7095048752
Aleego <Sankafoxe@gmail.com>
2025-06-03 14:14:02 +0300

    feat(entities/Currencies/model): add RTK query, slice and types for currencies

---
+++ src/entities/Currencies/model/api.ts
@@ -0,0 +1,22 @@
+import {api} from "@/shared/redux/api";
+import type {CurrencyTableData} from "./types";
+import {responseTransformer} from "@/entities/Currencies/lib/responseTransformer.ts";
+
+const currenciesApi = api.injectEndpoints({
+  endpoints: (create) => ({
+    getCurrencies: create.query<CurrencyTableData[], string>({
+      query: (date) => ({
+        url: '/rates',
+        params: {
+          periodicity: 0,
+          ondate: date,
+        }
+      }),
+      transformResponse: responseTransformer
+
+    })
+  }),
+  overrideExisting: true,
+});
+
+export default currenciesApi;
