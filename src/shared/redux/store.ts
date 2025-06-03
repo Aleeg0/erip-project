b393aff93cfb32356f1a230c8bcfeee7211d4118
Aleego <Sankafoxe@gmail.com>
2025-06-03 14:15:47 +0300

    feat(shared/redux): add currenciesSlice to store

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {currenciesSlice} from "@/entities/Currencies/model";
 import {api} from "./api.ts";
 import {useDispatch, useSelector} from "react-redux";
 
 export const store = configureStore({
   reducer: {
+    [currenciesSlice.name]: currenciesSlice.reducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
