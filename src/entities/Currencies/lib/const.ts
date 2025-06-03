9f9d18356d6152a659fe3bcc10726f0f297c74a9
Aleego <Sankafoxe@gmail.com>
2025-06-03 14:09:54 +0300

    feat(entities/Currencies/lib): added consts for currencies create map for getting country Code by currency Code and today info

---
+++ src/entities/Currencies/lib/const.ts
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+
+export const today = dayjs();
+
+export const currencyCountryMap: Record<string, string> = {
+  AED: "ae",
+  AMD: "am",
+  AUD: "au",
+  BGN: "bg",
+  BRL: "br",
+  CAD: "ca",
+  CHF: "ch",
+  CNY: "cn",
+  CZK: "cz",
+  DKK: "dk",
+  EUR: "eu",
+  GBP: "gb",
+  INR: "in",
+  IRR: "ir",
+  ISK: "is",
+  JPY: "jp",
+  KGS: "kg",
+  KWD: "kw",
+  KZT: "kz",
+  MDL: "md",
+  NOK: "no",
+  NZD: "nz",
+  PLN: "pl",
+  RUB: "ru",
+  SEK: "se",
+  SGD: "sg",
+  TRY: "tr",
+  UAH: "ua",
+  USD: "us",
+  VND: "vn",
+};
