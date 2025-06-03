41592744a066ed64e2986680dacba6a9f36804bc
Aleego <Sankafoxe@gmail.com>
2025-06-03 14:16:36 +0300

    feat(app): add providers for antd components, redux-store, react-router

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/styles/global.scss';
+import '@/shared/styles/global.scss';
+import {RouterProvider} from "react-router";
+import {router} from "@/shared/router";
+import {ConfigProvider} from "antd";
+import ruRU from 'antd/locale/ru_RU';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import {Provider} from "react-redux";
+import {store} from "@/shared/redux";
+
+dayjs.locale('ru');
+dayjs.extend(localizedFormat);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <ConfigProvider
+      locale={ruRU}
+      theme={{
+        "token": {
+          colorPrimary: "#1e3e7a",
+          colorInfo: "#1e3e7a",
+          colorLink: "#000000",
+          fontSize: 16,
+          fontSizeHeading1: 32,
+          fontSizeHeading2: 28,
+          fontSizeHeading3: 24,
+          fontSizeHeading4: 20,
+          borderRadius: 10,
+          boxShadow: "none",
+          boxShadowSecondary: "none",
+          wireframe: false,
+          sizeStep: 4
+        }
+      }}
+    >
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
+    </ConfigProvider>
   </StrictMode>,
 );
