dda0a87358dc6e58fedbf6fe7234ac36a7b0c68f
Aleego <Sankafoxe@gmail.com>
2025-06-05 00:24:19 +0300

    feat(shared/router): add CurrencyDynamic page to router

--- src/shared/router/router.tsx
+++ src/shared/router/router.tsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter} from "react-router";
-import {Layout, HomePage} from "@/pages";
+import {Layout, HomePage, CurrencyDynamicPage} from "@/pages";
 import {routeMap} from "@/shared/router/const.ts";
 
 export const router = createBrowserRouter([
@@ -7,7 +7,7 @@ export const router = createBrowserRouter([
     Component: Layout,
     children: [
       { index: true, Component: HomePage },
-      { path: routeMap['currencyDynamic'].path, Component: null},
+      { path: routeMap['currencyDynamic'].path, Component: CurrencyDynamicPage},
       { path: routeMap['converter'].path, Component: null}
     ],
   }
